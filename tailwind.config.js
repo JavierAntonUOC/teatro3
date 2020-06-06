ea142cb067811da972d23dbcbe71c06191aea8c7
javierantonmo <javierantonmoral0@gmail.com>
2020-06-06 18:55:08 +0200

    ConfirmacionFinalV!
    COMMIT que se hace después de terminar el sitio con TAILWIND  y comprobando que es responsivo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
+    screens: {
+      'sm': { 'max': '767px' },
+      'md': { 'min': '768px', 'max': '1023px' },
+      'lg': { 'min': '1024px', 'max': '1279px' },
+      'xl': { 'min': '1280px' },
+    },
     extend: {},
   },
   variants: {},
